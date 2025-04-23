c8493a95d6a8de7a96217ed82d7a5c0d0ffe9808
2024105070078 <2024105070078@aqs-lab-c-07.ifro.local>
2025-04-23 15:41:48 -0300

    segundo commit

--- src/main/java/ConversorTemperatura.java
+++ src/main/java/ConversorTemperatura.java
@@ -1,11 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-/**
- *
- * @author 2024105070078
- */
 public class ConversorTemperatura {
 
     public static void main(String[] args) {
